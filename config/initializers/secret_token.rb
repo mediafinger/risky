2e901e716d62a3c046943747640f31cddca8981b
Andreas Finger <webmaster@mediafinger.com>
2013-09-22 12:26:37 +0200

    New Ruby 2.0, Rails 4.0 app with Puma and Postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zugfahrt::Application.config.secret_key_base = '90b33746e3d1ad5c4805836ca9b40c802626df6eb050274a4d2ad952c6a32e200451132a51004df79c545c54cdf6e536b0676a25cefc0fa86d3eb2766ee1b249'
