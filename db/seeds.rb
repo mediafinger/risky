69a49d7750f07aec442144ef1298383d9b3f78c8
Andreas Finger <webmaster@mediafinger.com>
2013-09-23 18:51:58 +0200

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,64 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+if Rails.env == "development"
+  puts "Cleaning DB first ..."
+  Army.destroy_all
+  Card.destroy_all
+  Country.destroy_all
+  Game.destroy_all
+  #Neighbour.destroy_all
+  Player.destroy_all
+  Region.destroy_all
+  Round.destroy_all
+end
+
+puts "Creating Map"
+puts "  Creating Regions ..."
+# Region.create!(player_id: nil, bonus: 3, name: :Afrika)
+# Region.create!(player_id: nil, bonus: 7, name: :Asia)
+# Region.create!(player_id: nil, bonus: 2, name: :Australia)
+# Region.create!(player_id: nil, bonus: 5, name: :Europe)
+Region.create!(player_id: nil, bonus: 5, name: :North_America)
+Region.create!(player_id: nil, bonus: 2, name: :South_America)
+
+puts "  Creating Countries ..."
+brasil =    Country.create!(player_id: nil, region: Region.where(name: :South_America).first, name: :Brasil)
+colombia =  Country.create!(player_id: nil, region: Region.where(name: :South_America).first, name: :Colombia)
+mexico =    Country.create!(player_id: nil, region: Region.where(name: :North_America).first, name: :Mexico)
+peru =      Country.create!(player_id: nil, region: Region.where(name: :South_America).first, name: :Peru)
+
+
+puts "  Setting Neighbours ..."
+Neighbour.create!(country_id: brasil.id, neighbour_id: colombia.id)
+Neighbour.create!(country_id: brasil.id, neighbour_id: peru.id)
+Neighbour.create!(country_id: colombia.id, neighbour_id: brasil.id)
+Neighbour.create!(country_id: colombia.id, neighbour_id: mexico.id)
+Neighbour.create!(country_id: colombia.id, neighbour_id: peru.id)
+Neighbour.create!(country_id: mexico.id, neighbour_id: colombia.id)
+Neighbour.create!(country_id: peru.id, neighbour_id: brasil.id)
+Neighbour.create!(country_id: peru.id, neighbour_id: colombia.id)
+
+puts "Creating Game ..."
+Game.create!(name: :First_Duell)
+
+puts "  Creating Players ..."
+Player.create!(game: Game.where(name: :First_Duell).first, name: "Danny", rank: 1, color: :red)
+Player.create!(game: Game.where(name: :First_Duell).first, name: "Andy",  rank: 2, color: :blue)
+
+
+puts "Placing Armies on Map ..."
+x = 0
+countries = Country.all.to_a
+while x < countries.length do
+  Player.all.each do |player|
+    countries[x].player_id = player.id
+    countries[x].save!
+
+    Army.create!(player: player, country: countries[x], size: 1)
+
+    x += 1
+    break if x > countries.length
+  end
+end
+
+
+# Round.create!()
+# Card.create!()
